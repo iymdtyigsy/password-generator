15791c2d3dad5d7c72f24205c1918a53d2ec9143
iymdtyigsy <tant@stu.otc.school.nz>
2024-05-29 15:17:21 +1200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,9 +1,11 @@
 import customtkinter as ctk
-
+import PIL as pil
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue") 
 root = ctk.CTk()
 root.geometry("400x240")
+
+
 
 
 root.mainloop()
