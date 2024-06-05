2904c96f55aae249690146cd4864606da94cf8fb
iymdtyigsy <tant@stu.otc.school.nz>
2024-06-05 15:19:09 +1200

    update

--- GUI.py
+++ GUI.py
@@ -1,3 +1,4 @@
+import tkinter
 import customtkinter as ctk
 import PIL as pil
 ctk.set_appearance_mode("dark")
@@ -23,26 +24,66 @@ def enter():
 def copy():
     pass
 
-randompass_btn = ctk.CTkButton(root, text="random password", command=randompass)
-randompass_btn.pack(pady = 10)
+MainMenu_sideFrame1 = ctk.CTkFrame(master= root, 
+                                   fg_color="gray")
+MainMenu_sideFrame1.pack(side = "right", 
+                         padx = 20, 
+                         pady = 20,
+                         fill = "x")
 
-start_btn = ctk.CTkButton(root, text="start", command=start)
-start_btn.pack(pady = 10)
+MainMenu_sideFrame2 = ctk.CTkFrame(master= root, 
+                                   fg_color="gray")
+MainMenu_sideFrame2.pack(side= "right", 
+                         padx = 20, 
+                         pady = 20,
+                         fill = "x")
 
-quit_btn = ctk.CTkButton(root, text="quit", command=quit)
-quit_btn.pack(pady = 10)
+MainMenu_Mainframe = ctk.CTkFrame(master= root,
+                                  fg_color="gray",
+                                  width = 500)
+MainMenu_Mainframe.pack(side = "right",
+                        padx = 20,
+                        pady = 20,
+                        fill = "x",)
 
-tutorial_btn = ctk.CTkButton(root, text="tutorial", command=tutorial)
-tutorial_btn.pack(pady = 10)
+randompass_btn = ctk.CTkButton(master= MainMenu_sideFrame1, 
+                               text="random password", 
+                               command=randompass)
+randompass_btn.pack(pady = 10, 
+                    padx = 10)
 
-enter_btn = ctk.CTkButton(root, text="Enter", command=enter)
-enter_btn.pack(pady = 10)
+start_btn = ctk.CTkButton(master= MainMenu_sideFrame1, 
+                          text="start", 
+                          command=start)
+start_btn.pack(pady = 10, 
+               padx = 10)
 
-copy_btn = ctk.CTkButton(root, text="copy", command=copy)
-copy_btn.pack(pady = 10)
+quit_btn = ctk.CTkButton(master= MainMenu_sideFrame1, 
+                         text="quit", 
+                         command=quit)
+quit_btn.pack(pady = 10, 
+              padx = 10)
 
-MainMenu_Frame = ctk.CTkFrame(master= root, fg_color="light gray")
-MainMenu_Frame.pack(side = "top", padx = 10, pady = 10)
+tutorial_btn = ctk.CTkButton(master= MainMenu_sideFrame1, 
+                             text="tutorial", 
+                             command=tutorial)
+tutorial_btn.pack(pady = 10, 
+                  padx = 10)
+
+copy_btn = ctk.CTkButton(master= MainMenu_sideFrame2, 
+                         text="copy", 
+                         command=copy,
+                         width = 10)
+copy_btn.pack(pady = 5,
+              padx = 5)
+
+enter_btn = ctk.CTkButton(master= MainMenu_sideFrame2, 
+                          text="Enter", 
+                          command=enter,
+                          width = 10)
+enter_btn.pack(pady = 5, 
+               padx = 5)
+
 
 
 
