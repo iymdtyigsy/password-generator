d7b81e8d87d8d834dd6534040222b06d4f6876a9
iymdtyigsy <tant@stu.otc.school.nz>
2024-06-07 10:00:01 +1200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -4,8 +4,12 @@ import PIL as pil
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue") 
 root = ctk.CTk()
+root.title("Password generator")
 root.geometry("700x500")
+root.minsize(700, 500)
+root.maxsize(700, 500)    
 
+#functions
 def randompass():
     pass
 
@@ -24,28 +28,40 @@ def enter():
 def copy():
     pass
 
+#labels
+MainMenu_label = ctk.CTkLabel(master= root,
+                              text = "Password generator",
+                              text_color = "white",
+                              fg_color= "#01a6f8",
+                              width= 200,
+                              height= 25,
+                              font=("Bold", 20),
+                              corner_radius= 5)
+MainMenu_label.pack(padx = 10,
+                    pady = 10)
+
+#Frames
 MainMenu_sideFrame1 = ctk.CTkFrame(master= root, 
                                    fg_color="gray")
 MainMenu_sideFrame1.pack(side = "right", 
                          padx = 20, 
-                         pady = 20,
-                         fill = "x")
+                         pady = 20)
 
 MainMenu_sideFrame2 = ctk.CTkFrame(master= root, 
                                    fg_color="gray")
 MainMenu_sideFrame2.pack(side= "right", 
-                         padx = 20, 
-                         pady = 20,
-                         fill = "x")
+                         padx = 10, 
+                         pady = 10)
 
 MainMenu_Mainframe = ctk.CTkFrame(master= root,
                                   fg_color="gray",
                                   width = 500)
-MainMenu_Mainframe.pack(side = "right",
+MainMenu_Mainframe.pack(side = "left",
                         padx = 20,
                         pady = 20,
-                        fill = "x",)
+                        fill = "both")
 
+#buttons
 randompass_btn = ctk.CTkButton(master= MainMenu_sideFrame1, 
                                text="random password", 
                                command=randompass)
