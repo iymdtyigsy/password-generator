a1802a6b83ddd9574da6ec43c2249d732296ad29
iymdtyigsy <tant@stu.otc.school.nz>
2024-07-23 21:32:50 +1200

    update

--- GUI.py
+++ GUI.py
@@ -236,22 +236,27 @@ app.mainloop()
 class Mainwindow(ctk.CTk):
     def reset(self):
         pass
-    def sliding(self, value):
-        pass
-    def randompass(self):
-        pass
 
+    def sliding(self, value):
+        self.lengthlabel.configure(text=int(value))
+
+    def randompass(self):
+        self.delete_current()
+        self.loadRandPass()
+        
     def start(self):
         pass
     
     def Return(self):
-        pass
+        self.delete_current()
+        self.loadMainFrame()
 
     def quit(self):
-        pass
+        self.destroy()
 
     def tutorial(self):
-        pass
+        self.delete_current()
+        self.loadtutorial()
 
     def enter(self):
         self.answer_entry.configure(state="disabled")
@@ -266,6 +271,10 @@ class Mainwindow(ctk.CTk):
 
     def generate(self):
         pass
+
+    def delete_current(self):
+        for widget in self.MainFrameHolder.winfo_children():
+            widget.forget()
 
     def __init__(self):
         super().__init__()
@@ -298,7 +307,10 @@ class Mainwindow(ctk.CTk):
                                             height=400)
         self.MainFrameHolder.pack(padx=10, pady=10, fill="both", expand=True)
 
-        self.loadRandPass()
+        self.loadMainFrame()
+
+        
+
 
     def loadMainFrame(self):
         #Frames
@@ -324,7 +336,7 @@ class Mainwindow(ctk.CTk):
                                                     text="questions",
                                                     text_color="black",
                                                     fg_color="white",
-                                                    font=("Bold", 20),
+                                                    font=("Bold",18),
                                                     width=500,
                                                     height=50,
                                                     corner_radius=5)
@@ -367,7 +379,8 @@ class Mainwindow(ctk.CTk):
                             padx=10)
 
         self.quit_btn = ctk.CTkButton(self.MainMenu_sideFrame1, 
-                                      text="quit", 
+                                      text="quit",
+                                      hover_color="brown2", 
                                       command=self.quit)
         self.quit_btn.pack(pady=10, 
                            padx=10)
@@ -404,19 +417,27 @@ class Mainwindow(ctk.CTk):
     def loadRandPass(self):
         self.MainMenu_sideFrame1 = ctk.CTkFrame(self.MainFrameHolder, 
                                                 fg_color="light gray")
-        self.MainMenu_sideFrame1.pack(side="right", padx=20, pady=20)
+        self.MainMenu_sideFrame1.pack(side="right", padx=10, pady=10)
 
         self.MainMenu_Mainframe = ctk.CTkFrame(self.MainFrameHolder,
                                                fg_color="light grey",)
-        self.MainMenu_Mainframe.pack(side="left", padx=20, pady=20)
+        self.MainMenu_Mainframe.pack(side="left", padx=10, pady=10)
 
         self.MainMenu_MainframeFir = ctk.CTkFrame(self.MainMenu_Mainframe,
                                                fg_color="gray",)
-        self.MainMenu_MainframeFir.pack(padx=20, pady=20)
+        self.MainMenu_MainframeFir.pack(padx=10, pady=10)
         
         self.MainMenu_MainframeSEC = ctk.CTkFrame(self.MainMenu_Mainframe,
                                                   fg_color="gray")
-        self.MainMenu_MainframeSEC.pack(padx=20, pady=20)
+        self.MainMenu_MainframeSEC.pack(padx=10, pady=10)
+
+        self.SECframeSector1 =ctk.CTkFrame(self.MainMenu_MainframeSEC,
+                                           fg_color="gray")
+        self.SECframeSector1.pack(padx=10, pady=10)
+
+        self.SECframeSector2 =ctk.CTkFrame(self.MainMenu_MainframeSEC,
+                                           fg_color="gray")
+        self.SECframeSector2.pack(padx=10, pady=10)
 
         self.Mainframe_passwordlabel = ctk.CTkLabel(self.MainMenu_MainframeFir,
                                                     text="password",
@@ -430,12 +451,17 @@ class Mainwindow(ctk.CTk):
         self.Mainframe_passwordstrength = ctk.CTkProgressBar(
                                             self.MainMenu_MainframeFir)
         self.Mainframe_passwordstrength.pack(pady=10, padx=10)
-        
-        self.Slider = ctk.CTkSlider(self.MainMenu_MainframeSEC, 
+
+        self.passwordlength = ctk.CTkLabel(self.SECframeSector1,
+                                           text=("Password Length"))
+        self.passwordlength.pack()
+
+        self.Slider = ctk.CTkSlider(self.SECframeSector1, 
                                     from_=0, 
                                     to=50,
-                                    command=self.sliding)
-        self.Slider.pack(padx=10,pady=10)
+                                    command=self.sliding,
+                                    height = 20)
+        self.Slider.pack(side = "right")
 
         self.Slider.set(8)
                                             
@@ -443,32 +469,83 @@ class Mainwindow(ctk.CTk):
         self.generate_btn = ctk.CTkButton(self.MainMenu_sideFrame1, 
                                        text="Generate", 
                                        command=self.generate)
-        self.generate_btn.pack(pady=10, 
-                            padx=10)
-
-        self.quit_btn = ctk.CTkButton(self.MainMenu_sideFrame1, 
-                                      text="quit", 
-                                      command=self.quit)
-        self.quit_btn.pack(pady=10, 
-                           padx=10)
-
-        self.tutorial_btn = ctk.CTkButton(self.MainMenu_sideFrame1, 
-                                          text="tutorial", 
-                                          command=self.tutorial)
-        self.tutorial_btn.pack(pady=10, 
-                               padx=10)
+        self.generate_btn.pack(pady=10,padx=10)
 
         self.copy_btn = ctk.CTkButton(self.MainMenu_sideFrame1, 
                                       text="Copy", 
                                       command=self.copy)
-        self.copy_btn.pack(pady=10,
-                           padx=10)
+        self.copy_btn.pack(pady=10,padx=10)
 
         self.reset_btn = ctk.CTkButton(self.MainMenu_sideFrame1, 
                                       text="Reset", 
                                       command=self.reset)
-        self.reset_btn.pack(pady=10,
-                           padx=10)
+        self.reset_btn.pack(pady=10,padx=10)
+
+        self.tutorial_btn = ctk.CTkButton(self.MainMenu_sideFrame1, 
+                                          text="tutorial", 
+                                          command=self.tutorial)
+        self.tutorial_btn.pack(pady=10,padx=10)
+
+        self.returnbtn = ctk.CTkButton(self.MainMenu_sideFrame1,
+                                       text="return",
+                                       command=self.Return,)
+        self.returnbtn.pack(padx=10, pady=10)
+
+        self.quit_btn = ctk.CTkButton(self.MainMenu_sideFrame1, 
+                                      text="quit", 
+                                      hover_color="brown2",
+                                      command=self.quit)
+        self.quit_btn.pack(pady=10,padx=10)
+
+        self.lengthlabel = ctk.CTkLabel(self.SECframeSector1,
+                                        text=self.Slider.get(),
+                                        font=("Helvetica", 15),
+                                        fg_color="light gray",
+                                        text_color= "black",
+                                        corner_radius= 5)
+        self.lengthlabel.pack(side ="right")
+
+        checkSymbol = ctk.StringVar(value="off")
+        self.checkboxsmb = ctk.CTkCheckBox(self.SECframeSector2,
+                                        text="Symbols",
+                                        variable=checkSymbol,
+                                        onvalue="on",
+                                        offvalue="off")
+        self.checkboxsmb.pack(pady=5,padx=5)
+
+        checkNumber = ctk.StringVar(value="off")
+        self.checkboxnum = ctk.CTkCheckBox(self.SECframeSector2,
+                                        text="Numbers",
+                                        variable=checkNumber,
+                                        onvalue="on",
+                                        offvalue="off")
+        self.checkboxnum.pack(pady=5,padx=5)
+
+        checkLetters = ctk.StringVar(value="off")
+        self.checkboxlet = ctk.CTkCheckBox(self.SECframeSector2,
+                                        text="Letters",
+                                        variable=checkLetters,
+                                        onvalue="on",
+                                        offvalue="off")
+        self.checkboxlet.pack(pady=5,padx=5)
+
+        checkUp = ctk.StringVar(value="off")
+        self.checkboxup = ctk.CTkCheckBox(self.SECframeSector2,
+                                        text="Uppercase",
+                                        variable=checkUp,
+                                        onvalue="on",
+                                        offvalue="off")
+        self.checkboxup.pack(pady=5,padx=5)
+
+        checkLow = ctk.StringVar(value="off")
+        self.checkboxlow = ctk.CTkCheckBox(self.SECframeSector2,
+                                        text="Lowercase",
+                                        variable=checkLow,
+                                        onvalue="on",
+                                        offvalue="off")
+        self.checkboxlow.pack(pady=5,padx=5)
+
+
 
     def loadtutorial(self):
         self.tutorialFrame = ctk.CTkFrame(self.MainFrameHolder,
@@ -498,11 +575,6 @@ class Mainwindow(ctk.CTk):
         This is tutorial
         """)
         self.textbox.configure(state = ctk.DISABLED)
-
-
-    def delete_current(self):
-        for widget in self.MainFrameHolder.winfo_children():
-            widget.destory()
 
 if __name__ =="__main__":
     Mainwindow().mainloop()
